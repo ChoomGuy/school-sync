d7788adbc8a8872923bf6a48d6e3bdbe8ca1a7e3
ChoomGuy <124496289+ChoomGuy@users.noreply.github.com>
2025-01-15 11:51:06 +0000

    finished electoral college votes

--- stateVote.py
+++ stateVote.py
@@ -1,12 +1,7 @@
-def main():
-    repVote = 0
-    libVote = 0
-    option = 1
-    votes = openFile() # list in format 'STATE,0;a' 0 for undeclared, 1 for lib, a for rep
-    while 1<= option<=3:
-        print('1.\tSee Votes \n2.\tDeclare a state\n3.\t') 
+
 
 def stateIndex(a,state): # returns index
+    state = state.strip(' ')
     for i in range(len(a)):
         commaIndex = a[i].index(',')
         thing = (a[i])[0:commaIndex]
@@ -23,7 +18,7 @@ def declareState(state): # in the format 'STATE,0;0'
     return output
 
 def openFile():
-    votes = open(r"C:\Users\Zero\Documents\code\Python\state votes\ElectoralCollegeVotes.txt","r")
+    votes = open(r"/home/albiflora/Documents/GitHub/school-sync/ElectoralCollegeVotes.txt","r")
     votes = votes.read().split('\n')
     newList =[]
     for i in range(len(votes)):
@@ -34,6 +29,8 @@ def countVote(state): # in the format 'STATE,0;0'
     return int(state[state.index(',')+1:state.index(';')])
 
 def displayVotes(stateList):
+    print(f'{"State Name":<22}{"Votes":<6}{"Party"}')
+    print("-"*(22+6+1))
     for i in stateList:
         state = i[:i.index(',')]
         number = i[i.index(',')+1:i.index(';')]
@@ -46,3 +43,29 @@ def displayVotes(stateList):
             party = 'X'
         print(f'{state:<22}{number:<6}{party}')
 
+def main():
+    repVote = 0
+    libVote = 0
+    option = 1
+    votes = openFile() # list in format 'STATE,0;a' 0 for undeclared, 1 for lib, a for rep
+    while 1<= option<=3:
+        print('1.\tSee Votes \n2.\tDeclare a state\n\t') 
+        option = int(input())
+        if option == 1:
+            displayVotes(votes)
+        elif option == 2:
+            state = stateIndex(votes,input("Which state would you like to declare \n")) # this is some number
+            votes[state] = declareState(votes[state]) # this gets it in the form STATE,0;a
+            if (votes[state])[-1] == '1':
+                libVote += countVote(votes[state])
+            elif (votes[state])[-1] == 'a':
+                repVote += countVote(votes[state])
+            if repVote >= 270:
+                print("Republican candidate wins")
+                break
+            elif libVote >= 270:
+                print("BIDEN WINS NSAIODNSAOIDNOAISBDOADSUFB")
+                break
+
+
+main()
