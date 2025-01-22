0f80af902cbb68a6a288509169fb0fb9a52e099c
ChoomGuy <124496289+ChoomGuy@users.noreply.github.com>
2025-01-22 11:55:53 +0000

    Market added
    need to add buy

--- game/classes.py
+++ game/classes.py
@@ -1,8 +1,8 @@
 import random
 
 class plot:
-    def __init__(self):
-        self.crops = "None"
+    def __init__(self, crops):
+        self.crops = crops.name
         self.obstructed = True
     
     def checkUnlock(self):
@@ -51,7 +51,48 @@ class player:
             plotDict[choice].unlockPlot(self)
 
 
+class crop:
+    def __init__(self,name):
+        self.name = name
+        self.growth = random.randint(1,10)
+        self.rot = 0
 
+    def rotDevelopment(self):
+        if random.randint(1,20) == random.randint(1,20): # 5%
+            self.rot += 10
+        elif abs(random.randint(1,20)-random.randint(1,20)) == 1: # 9.5%
+            self.rot += 1
+
+
+class market:
+    def __init__(self, name, upperBound, lowerBound):
+        self.name=name
+        self.inventory = {"Wheat":random.randint(1,100),
+        "Potato":random.randint(1,100),
+        "Carrot":random.randint(1,100),
+        "Pumpkin":random.randint(1,100),
+        "Garlic":random.randint(1,100),
+        "Kale":random.randint(1,100)} # these are all seeds
+        self.prices = {"Wheat": random.randint(lowerBound,upperBound),
+        "Potato": random.randint(lowerBound,upperBound),
+        "Carrot": random.randint(lowerBound,upperBound),
+        "Pumpkin": random.randint(lowerBound,upperBound),
+        "Garlic": random.randint(lowerBound,upperBound)
+        }
+
+    def sellTo(self, crops: str, cropAmount: int, player):
+        if cropAmount > 0:
+            self.inventory[crops] += cropAmount
+            player.balance += int(self.prices[crop] * 1.45 * cropAmount)
+            print(f"You sold {cropAmount} {crop}s for {int(self.prices[crop] * 1.45 * cropAmount)} coins!")
+        else:
+            print("You can't sell nothing!")
+        
+    
+        
+    
+    
+    
         
 plotDict = {"plot0": plot(),
     "plot1": plot(),
@@ -60,6 +101,3 @@ plotDict = {"plot0": plot(),
     "plot4":plot(),
     "plot5":plot()}
 
-a = player()
-a.seeds["Wheat"]+= 3232323
-print(a.seeds)
