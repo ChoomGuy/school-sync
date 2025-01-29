f41b5668d1bad074affd780b0771ff1773f42ec1
ChoomGuy <124496289+ChoomGuy@users.noreply.github.com>
2025-01-29 11:55:26 +0000

    Implementing tutorial
    still need to add ---- around system messages

--- game/classes.py
+++ game/classes.py
@@ -2,6 +2,7 @@ import random
 
 class plot:
     def __init__(self, crops):
+        self.growthCap = crops.growthCap
         self.cropName = crops.name
         self.growthStage = crops.growthStage
         self.rotStage = crops.rot
@@ -17,17 +18,24 @@ class plot:
         if not self.checkUnlock():
             print("You can't access this plot")
         else:    
-            if self.crops == "None":
+            if self.cropName == "None":
                 print("There are no crops to harvest")
             else:
-                if not checkRot():
-                    cropYield = random.randint(8,12)
-                    print(f'You obtained {cropYield} {self.cropName}s!')
-                    player.inventory[self.cropName] += cropYield
-                    self.cropName = "None"
+                if self.growthStage < self.growthCap:
+                    print(f'The {self.cropName} is not ready to harvest!')
                 else:
-                    print("Unluckily, your crops have rotted away due to some unknown cause!")
-                    self.cropName = "None"
+                    if not self.checkRot():
+                        cropYield = random.randint(8,12)
+                        print('-'*30)
+                        print(f'You obtained {cropYield} {self.cropName.lower()}{'' if self.cropName in {"Garlic","Wheat","Kale"} else 's'}!')
+                        print(f'You also obtained {int(cropYield*1.4)} {self.cropName} seeds!')
+                        print('-'*30)
+                        player.inventory[self.cropName] += cropYield
+                        player.seeds[self.cropName] += int(cropYield * 1.4)
+                        self.cropName = "None"
+                    else:
+                        print("Unluckily, your crops have rotted away due to some unknown cause!")
+                        self.cropName = "None"
 
     def unlockPlot(self, player):
         self.obstructed = False
@@ -37,19 +45,25 @@ class plot:
         if not self.checkUnlock:
             print("You can't access this plot")
         else:
-            if player.seeds[crops] >= 1:
-                player.seeds[crops] -= 1
-                self.crops = crops
+            if self.cropName == "None":
+                if player.seeds[crops] >= 1:
+                    player.seeds[crops] -= 1
+                    self.crops = crops.name
+                else:
+                    print("You don't have enough seeds! ")
             else:
-                print("You don't have enough seeds! ")
+                print("You can't plant on an already planted plot! ")
 
     def checkCropType(self):
-        return self.crops.name
+        return self.cropName
 
     def checkRot(self):
         if self.rotStage >= 10:
             return True
         return False
+    
+    def checkGrowth(self):
+        return self.growthStage
 
 class player:
     def __init__(self):
@@ -66,20 +80,23 @@ class player:
         "Pumpkin": 0,
         "Garlic": 0,
         "Kale":0} 
-        self.ownedPlots = []
+        self.ownedPlots = {"Barn": plot(crop("Wheat"))}
 
     def buyPlot(self,choice):
         if self.balance - 10 > 0:
             self.balance -=10
-            print(f"You bought plot {choice}!")
-            choice = f'plot{choice}'            
-            plotDict[choice].unlockPlot(self)
+            print("You bought a plot!")           
+            print("Give the plot a name! \n")
+            name = input()
+            self.ownedPlots[name] = plot(crop("None"))
+            
         else:
             print("You're too broke! ")
 
 
 class crop:
     def __init__(self,name):
+        self.growthCap = random.randint(5,10)
         self.name = name
         self.growthStage = 0
         self.rot = 0
@@ -133,6 +150,13 @@ class market:
             player.seeds[purchase] += purchaseAmount
         else:
             print("You can't afford this!")
+
+    def seeInventory(self):
+        output = "There are"
+        for i in self.inventory:
+            output+= f'{self.inventory[i]} {i} seeds, '
+        output[-1]="."
+        print(output)
         
     
         
--- game/main.py
+++ game/main.py
@@ -1,5 +1,51 @@
 import classes as cl
 
-
 you = cl.player()
-print(you.balance)
+you.ownedPlots["Barn"].obstructed = False
+you.ownedPlots["Barn"].growthStage = 100
+
+
+superMarket = cl.market("Farm-mart",20,8)
+sketchyMarket = cl.market("Discounted Goods",30,1)
+
+def faq(): 
+    print("Type HELP to open this menu\n")
+    print("MARKET RELATED COMMANDS")
+    print(f"Type GOTO \x1B[3m{"marketName"}\x1B[0m to go to the market specified \nType BUY \x1B[3m{'item'}\x1B[0m to buy the item specified\nType SELL \x1B[3m{'item'}\x1B[0m to sell the item specified")
+    print("\n")
+    print("FARMING RELATED COMMANDS")
+    print(f"Type HARVEST \x1B[3m{'plotName'}\x1B[0m to harvest the specified plot\nType PLANT \x1B[3m{'plotName'}\x1B[0m to plant in the specified plot\nType CHECK \x1B[3m{'plotName'}\x1B[0m to check the crop in the specified plot and its growth stage")
+    print("\n")
+    print("PLAYER RELATED COMMANDS")
+    print(f"Type BALANCE to see your balance\nType CHECK \x1B[3m{'seed/inventory/plots'}\x1B[0m to see your seeds/inventory/owned plots \n")
+
+def ownedPlotList():
+    print(f'You own {len(you.ownedPlots)} plot{'s.' if len(you.ownedPlots) > 1 else '.'}')
+
+def checkPlot(plot_name):
+    print(f'There is {you.ownedPlots[plot_name].checkCropType().lower() if you.ownedPlots[plot_name].checkCropType != "None" else "nothing"} growing here!',end=' ')
+    if you.ownedPlots[plot_name].checkCropType != "None":
+        print(f"It is {'recently planted' if you.ownedPlots[plot_name].checkGrowth() < 1 else 'growing steadily' if 1<=you.ownedPlots[plot_name].checkGrowth()<you.ownedPlots[plot_name].growthCap else 'grown'}.")
+
+def tutorial():
+    ownedPlotList()
+    print("Let's go check on the plot! Type \'CHECK Barn\' to see what's happening!")
+    response = input()
+    while response.lower().strip() != "check barn":   
+        print("Let's go check on the plot! Type \'CHECK Barn\' to see what's happening!")
+        response = input()
+    checkPlot("Barn")
+    print("Now let's harvest it! Type \'HARVEST Barn\' to harvest it")
+    response = input()
+    while response.lower().strip() != "harvest barn":
+        print("Now let's harvest it! Type \'HARVEST Barn\' to harvest it")
+        response = input()
+    you.ownedPlots["Barn"].harvest(you)
+       
+
+def main():
+    response = ""
+    while response.upper != "QUIT":
+        pass
+
+tutorial()
